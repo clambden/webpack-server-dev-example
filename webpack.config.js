d54633a439f146c52ec6b510155d115df530a8d2
SCOTT\clambden <clambden@scottlogic.co.uk>
2018-10-16 10:10:44 +0100

    Use webpack to watch for changes and use start-server-webpack-plugin to lunch app after webpack build completes

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require("path");
+const StartServerPlugin = require("start-server-webpack-plugin");
 
 module.exports = {
     mode: 'development',
     target: 'node', // in order to ignore built-in modules like path, fs, etc.
     entry: [
-        "./src/index.js", 
+        "./src/index.js"
     ],    
+    plugins: [
+        new StartServerPlugin("bundle.js"),
+    ],
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "./build"),
